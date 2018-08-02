ee5a1f66859ddd4c84a53734b8a7a4ff107bb872
Emre Yilmaz <mail@emreyilmaz.me>
2018-08-02 23:29:01 +0300

    Release the 0.0.1. 🎉

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lightsteem',
+    version='0.0.1',
+    packages=find_packages('.'),
+    url='http://github.com/emre/lightsteem',
+    license='MIT',
+    author='emre yilmaz',
+    author_email='mail@emreyilmaz.me',
+    description='A light python client to interact with the STEEM blockchain',
+    install_requires=["requests"]
+)
