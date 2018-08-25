9acfc810f5ad3aa5b84dd479404c47354eb84f7f
Emre Yilmaz <mail@emreyilmaz.me>
2018-08-25 04:28:53 +0300

    Add custom chain support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lightsteem',
-    version='0.0.5',
+    version='0.0.6',
     packages=find_packages('.'),
     url='http://github.com/emre/lightsteem',
     license='MIT',
