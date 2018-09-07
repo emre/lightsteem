814973eafcac5488acbb44ab69353c194cc442e3
Emre Yilmaz <mail@emreyilmaz.me>
2018-09-07 18:47:18 +0300

    Version bump to 0.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lightsteem',
-    version='0.0.7',
+    version='0.0.8',
     packages=find_packages('.'),
     url='http://github.com/emre/lightsteem',
     license='MIT',
