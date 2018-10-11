7e4cc72f5152bffecd03e937d74c695a8f52de31
Emre Yilmaz <mail@emreyilmaz.me>
2018-10-11 20:14:45 +0300

    Version bump to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lightsteem',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages('.'),
     url='http://github.com/emre/lightsteem',
     license='MIT',
